796472afeb1a5a15c0dfbe478962c90f88f28eb8
bakemonostan <ehizomokhomion@gmail.com>
2023-10-10 23:53:05 +0100

    add clerk and auth routes

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <p>Hello Admin Dashboard</p>;
+  return (
+    <div>
+      <Button variant="destructive">Booty</Button>
+      <p>Hello Admin Dashboard</p>
+    </div>
+  );
 }
