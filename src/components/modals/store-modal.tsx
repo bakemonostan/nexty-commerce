699fafe738b68ca6a5e19d2e80ad5cc7d66c3873
bakemonostan <ehizomokhomion@gmail.com>
2023-10-15 22:27:36 +0100

    add forms, label input, zod and rhf

--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -1,9 +1,21 @@
 "use client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "../ui/modal";
 
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
   return (
     <Modal
       title="Create Store"
@@ -11,7 +23,7 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Create Store form
+      Future Create Store form
     </Modal>
   );
 };
