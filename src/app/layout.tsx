6f21d0af6fef1701ca807bb0cd3d12b3e0aba3f5
bakemonostan <ehizomokhomion@gmail.com>
2023-10-16 08:30:42 +0100

    init prisma and supabase db

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
 
 import "./globals.css";
+import prismadb from "@/lib/prismadb";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -5,17 +5,41 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "../ui/modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(2, {
+    message: "Required",
+  }),
 });
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
   });
 
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    // Create Store
+  };
   return (
     <Modal
       title="Create Store"
@@ -23,7 +47,40 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Future Create Store form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel> Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="E commerce" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button
+                  disabled={loading}
+                  variant="outline"
+                  onClick={storeModal.onClose}
+                >
+                  Cancel
+                </Button>
+                <Button disabled={loading} type="submit">
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 };
