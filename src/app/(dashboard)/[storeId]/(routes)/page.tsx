d67f12bb854cad31f3276f82deead5fe529ae17f
bakemonostan <ehizomokhomion@gmail.com>
2023-10-17 10:40:33 +0100

    updated modal, add components

--- src/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,22 @@
 import { UserButton } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
 
-const DashBoardPage = () => {
+interface Props {
+  params: {
+    storeId: string;
+  };
+}
+
+const DashBoardPage = async ({ params }: Props) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
   return (
     <>
       <div>This is a dashboard</div>
+      <p>Store name: {store?.name}</p>
       <UserButton />
     </>
   );
